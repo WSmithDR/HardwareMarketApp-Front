dcf6f43aa4280ca4a8905ccd05eb0f9ce9762eca
DavidMoralesz4 <jd3285201@gmail.com>
2024-01-16 16:51:57 -0500

    pushedMerch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 absolute top-0 ">
-        <p className="text-black">Home</p>
+        <p className="text-black">Home ||</p>
         <p className="text-black">Sign out</p>
         <p className="text-black">About</p>
     </nav>
