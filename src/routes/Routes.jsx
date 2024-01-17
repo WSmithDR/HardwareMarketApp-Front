a04c3e60f0209ebdbad76f6d2a10317437ae979b
DavidMoralesz4 <jd3285201@gmail.com>
2024-01-17 16:05:45 -0500

    pushLogoGoolge

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Routes as HardWareRoutes, Route} from 'react-router' 
-import WelcomePage from '../pages/welcomePage/WelcomePage'
-import { StartPage } from '../pages/StartPage/StartPage'
-import { Login } from '../pages/login/Login'
+import React from "react";
+import { Routes as HardWareRoutes, Route } from "react-router";
+import WelcomePage from "../pages/welcomePage/WelcomePage";
+import { StartPage } from "../pages/StartPage/StartPage";
+import { Login } from "../pages/login/Login";
+import { Forms } from "../pages/FormsPage/Forms";
 
 const Routes = () => {
   return (
-   <>
-   <HardWareRoutes>
-    <Route path='/' element = {<WelcomePage/>}></Route>
-    <Route path= "/startPage" element = {<StartPage/>}></Route>
-    <Route path= "/iniciarSesion" element = {<Login />}></Route>
-   </HardWareRoutes>
-   </>
-  )
-}
+    <>
+      <HardWareRoutes>
+        <Route path="/" element={<WelcomePage />}></Route>
+        <Route path="/startPage" element={<StartPage />}></Route>
+        <Route path="/iniciarSesion" element={<Login />}></Route>
+        <Route path="/formulario" element={<Forms />}></Route>
+      </HardWareRoutes>
+    </>
+  );
+};
 
-export default Routes
+export default Routes;
