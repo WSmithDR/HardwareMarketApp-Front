2698180eeabdcaf7d5ca8603e620c821d605c5d7
carniiga <fernandezagustin98@.com>
2024-01-22 12:30:03 -0300

    modifiqué navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ export const Navbar = () => {
     <nav className="flex justify-between items-center p-4 absolute top-0 ">
         <div className="container mx-auto flex justify-between items-center">
         <div className="space-x-10">
-          <a href="#" className="text-black font-josefin shadow-xl">Home</a>
-          <a href="#" className="text-black font-josefin shadow-xl">About</a>
-          <a href="#" className="text-black font-josefin shadow-xl">Contact</a>
-          <a href="#" className="text-black font-josefin shadow-xl" onClick={() => navigate("/login")}>Iniciar Sesion</a>
+          <a href="#" className="text-black font-josefin shadow-xl">Inicio</a>
+          <a href="#" className="text-black font-josefin shadow-xl">Sobre Nosotros</a>
+          <a href="#" className="text-black font-josefin shadow-xl">Contacto</a>
+          <a href="#" className="text-black font-josefin shadow-xl" onClick={() => navigate("/login")}>Iniciar sesion</a>
         </div>
       </div>
     </nav>
