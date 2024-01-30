dcb75816bfbd215585e19415f9efedb9f2192521
DavidMoralesz4 <jd3285201@gmail.com>
2024-01-30 13:39:01 -0500

    masteSaumerio

--- src/features/store/containers/StoreContainer.jsx
+++ src/features/store/containers/StoreContainer.jsx
@@ -1,11 +1,11 @@
-import { Products } from "./products/Products"
-import { SideBar } from "./sidebar/SideBar"
+import { Products } from "./products/Products";
+import { SideBar } from "./sidebar/SideBar";
 
-export const StoreContainer = () =>{
-    return(
-        <div className="grid grid-cols-12">
-            <SideBar className="col-[1/3] grid justify-center"/>
-            <Products className="col-[3/e]"/>
-        </div>
-    )
-}
+export const StoreContainer = () => {
+  return (
+    <div className="grid grid-cols-12">
+      <SideBar className="col-[1/3] grid justify-center" />
+      <Products className="col-[3/e]" />
+    </div>
+  );
+};
--- src/features/store/roots/StoreScreen.jsx
+++ src/features/store/roots/StoreScreen.jsx
@@ -2,13 +2,12 @@ import Footer from "../../../components/footer/Footer";
 import { StoreContainer } from "../containers/StoreContainer";
 import { StoreNavBar } from "../containers/navigation/StoreNavBar";
 
-
 export const StoreScreen = () => {
   return (
-    <div className="flex flex-col  font-josefin">
-      <StoreNavBar/>
-      <StoreContainer/>
-      <Footer/>
+    <div className="flex flex-col  font-josefin md:flex md:flex-col">
+      <StoreNavBar />
+      <StoreContainer />
+      <Footer />
     </div>
   );
 };
