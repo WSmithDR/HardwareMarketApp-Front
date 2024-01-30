cbca03d2bfa0dcafe0396ead9532f6fb531efffa
DavidMoralesz4 <jd3285201@gmail.com>
2024-01-30 12:10:20 -0500

    settingsGeneral

--- src/features/store/containers/StoreContainer.jsx
+++ src/features/store/containers/StoreContainer.jsx
@@ -1,11 +1,11 @@
-import { Products } from "./products/Products"
-import { SideBar } from "./sidebar/SideBar"
+import { Products } from "./products/Products";
+import { SideBar } from "./sidebar/SideBar";
 
-export const StoreContainer = () =>{
-    return(
-        <div className="grid grid-cols-12">
-            <SideBar/>
-            <Products/>
-        </div>
-    )
-}
+export const StoreContainer = () => {
+  return (
+    <div className="grid grid-cols-12 p-5">
+      <SideBar />
+      <Products />
+    </div>
+  );
+};
