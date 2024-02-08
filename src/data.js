e5f981fc9782b38696c0636381eec5a5a6614dcd
DavidMoralesz4 <jd3285201@gmail.com>
2024-02-07 19:32:12 -0500

    Update-Funcional-App

--- src/data.js
+++ src/data.js
@@ -1,19 +1,10 @@
 import axios from "axios";
 
 export const productsFetch = async () => {
-<<<<<<< HEAD
-    const res = await axios.get(
-        "https://hardwaremarket-backen.onrender.com/v1/api/products"
-    );
-    const data = res.data.data.payload;
-    console.log(data)
-    return data;
-=======
   const res = await axios.get(
     "https://hardwaremarket-backen.onrender.com/v1/api/products"
   );
-  //   console.log(res);
   const data = res.data.data.payload;
+  console.log(data);
   return data;
->>>>>>> rame
 };
