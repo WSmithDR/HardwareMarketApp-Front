600dd8efb5b8ba0b26e68f52ac6599130f658873
Elliot <unforriven@gmail.com>
2024-02-08 17:29:38 -0300

    Carrito

--- src/features/store/components/Cart/Cart.jsx
+++ src/features/store/components/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import { PropTypes } from 'prop-types'
 export const Cart = ({cartOpen, handleCart}) => {
 
     const closedStyle = "absolute top-[115px] right-0 bg-white w-[300px] h-[600px] p-8 ml-2 mr-4 mt-[-10px] shadow-[0_3px_2px_0_rgba(0,0,0,0.3)] ease-in duration-200 translate-x-[170rem]"
-    const openStyle = "absolute top-[115px] right-0 bg-white w-[300px] h-[600px] p-8 ml-2 mr-4 mt-[-10px] shadow-[0_3px_2px_0_rgba(0,0,0,0.3)] rounded-b-[20px] ease-out duration-200 translate-x-[0rem] max-[768px]:m-[9px] max-[768px]:translate-x-[-0.2rem]"
+    const openStyle = "absolute top-[115px] right-0 bg-white w-[300px] h-[600px] p-8 ml-2 mr-4 mt-[-10px] shadow-[0_3px_2px_0_rgba(0,0,0,0.3)] rounded-b-[20px] ease-out duration-200 translate-x-[0rem] max-[768px]:m-[9px] max-[768px]:translate-x-[-0.2rem] max-[768px]:translate-y-[-2.8rem] max-[700px]:w-[95%] max-[768px]:"
 
     
     //Lo hice de esta forma para ver como manejar la suma de los precio
