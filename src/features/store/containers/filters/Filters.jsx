12732f1d24b3a1f0a8077ad597506263046623bb
DavidMoralesz4 <jd3285201@gmail.com>
2024-02-21 12:28:43 -0500

    settings

--- src/features/store/containers/filters/Filters.jsx
+++ src/features/store/containers/filters/Filters.jsx
@@ -18,18 +18,18 @@ export const Filters = () => {
     <div className=" w-[400px] h-fit flex flex-col md:px-20 md:mt-10 gap-10 mt-8">
       <div className=" max-[425px]:w-[350px] max-[425px]:flex justify-between">
         <div>
-          <h1 className="text-[20px]">Tarjetas de video</h1>
-          <p>{products.length} Resultados</p>
+          <h1 className="text-[20px] font-josefin">Tarjetas de video</h1>
+          <p className="font-josefin">{products.length} Resultados</p>
         </div>
       </div>
       <div
-        className="flex items-center w-[100%] h-[40px] shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-2 cursor-pointer"
+        className="flex items-center w-[100%] h-[40px] shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-2 cursor-pointer font-josefin"
         onClick={() => handleCategories()}
       >
         Categories
       </div>
       <Categories hidden={hiddenCategories} />
-      <div className="bg-[#C8C7C7] h-[40px] w-[100%] rounded-[10px] flex items-center p-4 justify-between ">
+      <div className="bg-[#C8C7C7] h-[40px] w-[100%] rounded-[10px] flex items-center p-4 justify-between font-josefin">
         <p>Envio Gratis</p>
         <SwitchButton />
       </div>
@@ -44,29 +44,29 @@ export const Filters = () => {
           hiddenFilter ? "hidden" : "flex"
         }`}
       >
-        <p>Filtrado de precios</p>
+        <p className="font-josefin">Filtrado de precios</p>
         <div className=" h-[40px] flex justify-between">
           <select className="h-[30px] w-[60px] rounded-[10px] flex text-center">
-            <option>0</option>
-            <option>100</option>
-            <option>500</option>
-            <option>1000</option>
-            <option>1500</option>
+            <option className="font-josefin">0</option>
+            <option className="font-josefin">100</option>
+            <option className="font-josefin">500</option>
+            <option className="font-josefin">1000</option>
+            <option className="font-josefin">1500</option>
           </select>
           <div className="w-[40px] h-[20px] border-b-2 border-black"></div>
           <select className="h-[30px] w-[60px] rounded-[10px]">
-            <option>100</option>
-            <option>500</option>
-            <option>1000</option>
-            <option>1500</option>
-            <option>2000</option>
+            <option className="font-josefin">100</option>
+            <option className="font-josefin">500</option>
+            <option className="font-josefin">1000</option>
+            <option className="font-josefin">1500</option>
+            <option className="font-josefin">2000</option>
           </select>
         </div>
       </div>
       <div className="hidden sm:flex sm:flex-col">
-        <h3 className="text-[20px]">Productos</h3>
-        <p className="cursor-pointer">Nuevos</p>
-        <p className="cursor-pointer">Usados</p>
+        <h3 className="text-[20px] font-josefin">Productos</h3>
+        <p className="cursor-pointer font-josefin">Nuevos</p>
+        <p className="cursor-pointer font-josefin ">Usados</p>
       </div>
     </div>
   );
