5ecaa476d56a5d78aa349a18a0e69dd8a60b3f3a
carniiga <fernandezagustin98@.com>
2024-02-21 18:20:24 -0300

    url login

--- src/features/login/loginUtils.js
+++ src/features/login/loginUtils.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const loginFetch = async(values) => {
     
-  const res = await axios.post("https://hardwaremarketapp-back.onrender.com/v1/api/sessions/login",values)
+  const res = await axios.post("https://hardwaremarketapp-back.onrender.com//v1/api/sessions/login",values)
  
   return res.data
 }
